2f27363fcc78588a163a32d38d4714289d8d8735
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-03-19 15:22:30 +0000

    Don't fail the documentation build on warnings

--- noxfile.py
+++ noxfile.py
@@ -37,7 +37,7 @@ def docs(session):
     session.install("-r", "docs/requirements.txt")
 
     # Generate documentation into `build/docs`
-    session.run("sphinx-build", "-W", "-b", "dirhtml", "-v", "docs/", "build/docs")
+    session.run("sphinx-build", "-b", "dirhtml", "-v", "docs/", "build/docs")
 
 
 @nox.session(reuse_venv=True)
