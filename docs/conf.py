b68997adb6437816159257a9568563322fdb66e3
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-03-19 14:42:19 +0000

    Enable the "not ready for use" announcement
    This might help deter people from trying to use this theme.

--- docs/conf.py
+++ docs/conf.py
@@ -91,10 +91,10 @@ html_theme = "lutra"
 html_title = "Lutra"
 
 html_theme_options = {
-    # "announcement": (
-    #     "This theme is under development and not in a state that it can be used by 'real' projects."
-    # ),
-    # "announcement_style": "warning",
+    "announcement": (
+        "This theme is under development and not in a state that it can be used by 'real' projects."
+    ),
+    "announcement_style": "warning",
     "primary_color": "blue",
     "accent_color": "violet",
     "hero": {
